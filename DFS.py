8a4b1bde3dc28fa049dab1329059ac31a4c3a62a
kalebert <evanschulte9@gmail.com>
2020-11-12 12:51:21 -0600

    Setting up Repo

---
+++ DFS.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+#import our CSV file 
+df = pd.read_csv('2019.csv')
+
+
+#drop uneccessary colums
+df.drop(['Rk', '2PM', '2PP', 'FantPt', 'DKPt', 'FDPt', 'VBD', 'PosRank', 'OvRank', 'PPR', 'Fmb', 'GS'], axis=1, inplace=True)
+
+#fix name formatting
+df['Player'] = df['Player'].apply(lambda x: x.split('*')[0]).apply(lambda x: x.split('\\')[0])
+
+#rename columns
+df.rename({
+    'TD': 'PassingTD',
+    'TD.1': 'RushingTD',
+    'TD.2': 'RecevingTD',
+    'TD.3': 'TotalTD',
+    'Yds': 'PassingYDs',
+    'Yds.1': 'RushingYDs',
+    'Yds.2': 'RecevingYDs',
+    'Att': 'PassingAtt',
+    'Att.1': 'RushingAtt'
+}, axis=1, inplace=True)
+
+#seperate dataframes based off position
+rb_df = df[df['FantPos'] == 'RB']
+qb_df = df[df['FantPos'] == 'QB']
+wr_df = df[df['FantPos'] == 'WR']
+te_df = df[df['FantPos'] == "TE"]
+
+df.to_html('DFS.html')
