968e34f34f8e6395186b95fb5f888660a3ba1419
kalebert <evanschulte9@gmail.com>
2020-11-16 14:06:19 -0600

    Refactor and ConCat DataFrame

--- df_heatmap.py
+++ df_heatmap.py
@@ -6,7 +6,6 @@ from matplotlib import pyplot as plt
 #import our CSV file 
 df = pd.read_csv('2019.csv')
 
-
 #drop uneccessary colums
 df.drop(['Rk', '2PM', '2PP', 'FantPt', 'DKPt', 'FDPt', 'VBD', 'PosRank', 'OvRank', 
         'PPR', 'Fmb', 'GS', 'Age', 'Tgt', 'Y/A', 'Att', 'Att.1', 'Cmp', 'Y/R'], axis=1, inplace=True)
@@ -44,4 +43,34 @@ qb_df = df[df['FantPos'] == 'QB']
 wr_df = df[df['FantPos'] == 'WR']
 te_df = df[df['FantPos'] == "TE"]
 
-print(rb_df.head())
+# Sort players by position rank on respected team
+
+def get_top_players(df, n):
+    return df.groupby('Tm').apply(lambda x: x.nlargest(n, ['FantasyPoints']).min()).reset_index(drop=True)
+
+qb_df = get_top_players(qb_df, 1)
+te_df = get_top_players(te_df, 1)
+rb1_df = get_top_players(rb_df, 1)
+rb2_df = get_top_players(rb_df, 2)
+wr1_df = get_top_players(wr_df, 1)
+wr2_df = get_top_players(wr_df, 2)
+wr3_df = get_top_players(wr_df, 3)
+ 
+new_names = {
+    'QB1': qb_df,
+    'TE1': te_df,
+    'RB1': rb1_df,
+    'RB2': rb2_df,
+    'WR1': wr1_df,
+    'WR2': wr2_df,
+    'WR3': wr3_df
+}
+
+for name, new_df in new_names.items():
+    new_df.rename({'FantasyPoints/GM': name}, axis=1, inplace=True)
+    new_df.drop(['FantPos', 'FantasyPoints'], axis=1, inplace=True)
+    new_df.set_index('Tm', inplace=True)
+
+df = pd.concat([qb_df, te_df, rb1_df, rb2_df, wr1_df, wr2_df, wr3_df], axis=1)
+
+print(df.head())
