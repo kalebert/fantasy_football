7cb7281e87e6121e148b93f4b98eb2e924c1b2cf
kalebert <evanschulte9@gmail.com>
2020-11-19 15:03:48 -0600

    Setup WebScraping

---
+++ webscraping.py
@@ -0,0 +1,34 @@
+from sys import argv
+from requests import get
+import pandas pd 
+from bs4 import BeautifulSoup
+
+year = input('What season? (1999-2019')
+week = input('What week of the {0} season?'.format(year))
+
+year, week = int(year), int(week)
+
+passingURL = """ 
+https://www.pro-football-reference.com/play-index/pgl_finder.cgi?request=1&match=game&year_min={year}&year_max={year}&season_start=1&season_end=-1&age_min=0&age_max=99&game_type=A&league_id=&team_id=&opp_id=&game_num_min=0&game_num_max=99&week_num_min={week}&week_num_max={week}&game_day_of_week=&game_location=&game_result=&handedness=&is_active=&is_hof=&c1stat=pass_att&c1comp=gt&c1val=1&c2stat=&c2comp=gt&c2val=&c3stat=&c3comp=gt&c3val=&c4stat=&c4comp=gt&c4val=&order_by=pass_rating&from_link=1
+""".format(year=year, week=week)
+
+receivingURL = """
+https://www.pro-football-reference.com/play-index/pgl_finder.cgi?request=1&match=game&year_min={year}&year_max={year}&season_start=1&season_end=-1&age_min=0&age_max=99&game_type=A&league_id=&team_id=&opp_id=&game_num_min=0&game_num_max=99&week_num_min={week}&week_num_max={week}&game_day_of_week=&game_location=&game_result=&handedness=&is_active=&is_hof=&c1stat=rec&c1comp=gt&c1val=1&c2stat=&c2comp=gt&c2val=&c3stat=&c3comp=gt&c3val=&c4stat=&c4comp=gt&c4val=&order_by=rec_yds&from_link=1
+""".format(year=year, week=week)
+
+rushingURL = """
+https://www.pro-football-reference.com/play-index/pgl_finder.cgi?request=1&match=game&year_min={year}&year_max={year}&season_start=1&season_end=-1&age_min=0&age_max=99&game_type=A&league_id=&team_id=&opp_id=&game_num_min=0&game_num_max=99&week_num_min={week}&week_num_max={week}&game_day_of_week=&game_location=&game_result=&handedness=&is_active=&is_hof=&c1stat=rush_att&c1comp=gt&c1val=1&c2stat=&c2comp=gt&c2val=&c3stat=&c3comp=gt&c3val=&c4stat=&c4comp=gt&c4val=&order_by=rush_yds&from_link=1
+""".format(year=year, week=week)
+
+urls = {
+    'Passing': passingURL,
+    'Receiving': receivingURL,
+    'Rushing': rushingURL
+}
+
+dfs = []
+
+defColumnSettings = {
+    'axis': 1,
+    'inplace': True
+}
